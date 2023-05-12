88b2e4b83216b9ae272f7dea1759513a49905276
Lucas Cajado <lucascajado30@gmail.com>
2023-05-12 12:16:37 -0300

    conexao.php

--- conexao.php
+++ conexao.php
@@ -18,11 +18,31 @@ $sql .= "CREATE TABLE IF NOT EXISTS usuarios(
             nome Varchar(100),
             login Varchar(30),
             senha Varchar(80),
-            cargo ENUM('operador', 'gerente', 'diretor'),
+            cargo ENUM('operador', 'gerente', 'diretor') NOT NULL,
             Primary Key (ID)) ENGINE = MyISAM";
 
+$sql .= "CREATE TABLE IF NOT EXISTS historico(
+            Codigo_Venda Int UNSIGNED ZEROFILL NOT NULL,
+            Data Varchar(30) NOT NULL,
+            ID_Loja Varchar(50) NOT NULL,
+            Produtos Text NOT NULL,
+            Quantidades Text NOT NULL,
+            Valor_Total Decimal(10,2) NOT NULL,
+            Primary Key(Codigo_Venda)) ENGINE = MyISAM"
+ 
+$sql .= "CREATE TABLE IF NOT EXISTS idlojas(
+            ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+            Lojas Varchar(150) NOT NULL,
+            Primary Key(ID)) ENGINE = MyISAM"
+
+$sql .= "CREATE TABLE IF NOT EXISTS produtosdb(
+            ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+            Produtos Varchar(100) NOT NULL,
+            Valores Decimal(10,2) NOT NULL,
+            Primary Key(ID)) ENGINE = MyISAM"
+
 if(mysqli_multi_query($mysqli, $sql) === FALSE){
-    echo 'Erro ao criar a tabela!' . mysqli_error($mysqli);
+    echo 'Erro ao criar o banco de dados: ' . mysqli_error($mysqli);
 }
 
 ?>
